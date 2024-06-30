7ebfad93f987790d1dcad5b5b1096058277f673b
PrinceLumpy <princelumpypackmule@gmail.com>
2024-06-30 10:32:32 -0400

    Track and display the number of days taken to reach the goal

--- CounterApp.py
+++ CounterApp.py
@@ -4,28 +4,31 @@ import os.path
 with open("help.txt") as f:
     HELP_TEXT = f.read()
 
-weekly_goal = 0
-weekly_total = 0
-daily_goal = 0
-daily_total = 0
+weekly_goal = int()
+weekly_total = int()
+daily_goal = int()
+daily_total = int()
+days_taken = int()
 
 def fetch_data():
     try:
         with open("goals-and-totals.json", 'r') as f:
             data = json.load(f)
-            global weekly_goal, daily_goal, weekly_total, daily_total
+            global weekly_goal, daily_goal, weekly_total, daily_total, days_taken
             weekly_goal = data["weekly_goal"]
             daily_goal = data["daily_goal"]
             weekly_total = data["weekly_total"]
             daily_total = data["daily_total"]
+            days_taken = data["days_taken"]
     except FileNotFoundError:
         print("goals-and-totals.json file not found")
 
 def print_data():
     print(f"{'~'*52}")
     print(f"|{'Weekly Total: ':12}{weekly_total:4}/{weekly_goal:4}{'Daily Total:':>19}{daily_total:>4}/{daily_goal:3}|")
+    print(f"|{'Day: ':5}{days_taken:4}{' '*41}|")
     daily_success_message="You completed  your daily goal!"
-    weekly_success_message="Congratulations! You've reached your weekly goal!"
+    weekly_success_message=f"Congratulations! You've reached your goal in {days_taken} days!"
     if weekly_total >= weekly_goal:
         print(f"|{weekly_success_message:^50}|")
     elif daily_total >= daily_goal:
@@ -33,7 +36,7 @@ def print_data():
     print(f"{'~'*52}")
         
 def command_handler(command):
-    global weekly_goal, weekly_total, daily_goal, daily_total
+    global weekly_goal, weekly_total, daily_goal, daily_total, days_taken
     # general
     if command == "quit":
         quit()
@@ -50,11 +53,19 @@ def command_handler(command):
         if response == "" or response.lower() == "y":
             weekly_total = 0
             daily_total = 0
+            days_taken = 0
     elif command == "new day":
         print("are you sure you want to start a new day? (Y/n)")
         response = input()
         if response == "" or response.lower() == "y":
-            daily_total = 0
+            with open("goals-and-totals.json", 'r') as f:
+                data = json.load(f)
+                key = f"day{days_taken}_reps"
+                data[key] = daily_total
+                data["daily_total"] = 0
+                data["days_taken"] += 1
+            save_data(data)
+            return False
     # set goals
     elif command.startswith("reset goals"):
         daily_goal = 600
@@ -125,7 +136,8 @@ def save_data(data = None):
             "weekly_goal": weekly_goal,
             "daily_goal": daily_goal,
             "weekly_total": weekly_total,
-            "daily_total": daily_total
+            "daily_total": daily_total,
+            "days_taken": days_taken
         }
         json.dump(data, f)
 
@@ -144,7 +156,8 @@ if __name__ == '__main__':
                 "weekly_goal": 3000,
                 "daily_goal": 600,
                 "weekly_total": 0,
-                "daily_total": 0
+                "daily_total": 0,
+                "days_taken": 1
             })
     # fetch and print
     fetch_data()
